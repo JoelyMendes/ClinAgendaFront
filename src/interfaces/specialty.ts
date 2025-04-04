802a497e557d06a507f60100b82adb96cf0ee296
JoelyMendes <joely.vboas@gmail.com>
2025-04-03 22:03:45 -0300

    teste

---
+++ src/interfaces/specialty.ts
@@ -0,0 +1,20 @@
+export interface ISpecialty {
+  id: number
+  name: string
+  scheduleDuration: string
+}
+
+export type GetSpecialtyListRequest = {
+  itemsPerPage: number
+  page: number
+  name: ISpecialty['name']
+}
+
+export type GetSpecialtyListResponse = {
+  total: number
+  items: ISpecialty[]
+}
+
+export type SpecialtyForm = {
+  name: ISpecialty['name']
+}
